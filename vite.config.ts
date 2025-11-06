3974d88e51845b6276409654fd2aa21da2cbfb31
Timothy Mwamondwe <robertkapira@gmail.com>
2025-11-07 01:10:20 +0200

    Fix Vite config to work with both localhost and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/tech/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/tech/' : '/',
   server: {
     host: "::",
     port: 8080,
@@ -15,4 +15,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
